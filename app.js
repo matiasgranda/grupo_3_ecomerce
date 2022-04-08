3bb885c4ff29c0624c9553955d1a4a8aca7ed489
Matuca <matigranda93@gmail.com>
2022-04-08 19:55:20 -0300

    Primer push del boceto HTML y Css

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicPath = (path.resolve(__dirname, "./public"));
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
+app.listen(3000, () => {
+    console.log("Server running.");
+    });
