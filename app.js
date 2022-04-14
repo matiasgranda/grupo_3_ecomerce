64d31d0ebcafee2db4eca26b89fa261278ad1290
Matuca <86964479+Matuca@users.noreply.github.com>
2022-04-14 19:51:05 -0300

    Agregado el path a pagina de registro en app.js

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/login", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
 });
 
+app.get("/registro", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/registro.html"))
+});
+
 app.listen(3000, () => {
     console.log("Server running.");
-    });
+});
