1468b2d4ac35cab2321716113309fcae1db80155
jldefigueiredo <jldefigueiredo@gmail.com>
2022-04-25 14:10:42 -0300

    Acomodado

--- app.js
+++ app.js
@@ -7,24 +7,10 @@ const app = express();
 const publicPath = (path.resolve(__dirname, "./public"));
 app.use(express.static(publicPath));
 
+/****************** Rutas *****************************/
 app.use("/product",productRoutes)
 app.use('/',mainRoutes);
-
-/*app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/index.html"))
-});*/
-app.get("/cesta", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/cesta.html"))
-});
-
-app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"))
-});
-
-app.get("/registro", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/registro.html"))
-});
-
+/******************************************************/
 
 app.listen(3000, () => {
     console.log("Server running.");
--- routes/main.js
+++ routes/main.js
@@ -9,5 +9,16 @@ console.log(__dirname, "./views/home.html");
 router.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../views/index.html"))
 });
+router.get("/cesta", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../views/cesta.html"))
+});
+
+router.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../views/login.html"))
+});
+
+router.get("/registro", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../views/registro.html"))
+});
 
 module.exports=router;
