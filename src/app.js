4b679a8533ddd5e8c46fb44d1595489a52487de4
jldefigueiredo <jldefigueiredo@gmail.com>
2022-04-27 08:03:59 -0300

    Correcciones

--- src/app.js
+++ src/app.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD:app.js
-const express = require("express");
-const path = require("path");
-let productRoutes=require("./routes/products.js");
-let mainRoutes=require("./routes/main.js");
-const app = express();
-
-const publicPath = (path.resolve(__dirname, "./public"));
-app.use(express.static(publicPath));
-app.set('view engine','ejs');
-/****************** Rutas *****************************/
-app.use("/product",productRoutes)
-app.use('/',mainRoutes);
-/******************************************************/
-
-app.listen(3000, () => {
-    console.log("Server running.");
-=======
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -35,5 +17,4 @@ app.use("/", mainRoutes);
 
 app.listen(3000, () => {
     console.log("Server running.");
->>>>>>> 0b8177ac138d506870f95a414f62ed72836df56a:src/app.js
 });
