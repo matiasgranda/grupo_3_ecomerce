f8afd25d949b5ec7c65ffe891d5e51f9209befd8
José Luis de Figueiredo <jldefigueiredo@gmail.com>
2022-05-11 10:14:24 -0300

    correccion de calificaciones, comentarios,descipcion de articulo, metodos en action.js y varias mas

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,55 +1,46 @@
 const path = require("path");
 const fs = require("fs");
-const actions = require("../data/actions")
-const {
-  parse
-} = require("path");
+const actions = require("../data/actions");
+const { parse } = require("path");
 let productos = JSON.parse(fs.readFileSync("./src/data/productos.json"));
 
-function retornarNombreArchivo(ruta){
-  let arrPath=ruta.split("img\\");
-  return "/img/"+arrPath[arrPath.length-1];
-}
-
 let productsController = {
-
   product: (req, res) => {
     let productoSeleccionado;
     for (let i = 0; i < productos.length; i++) {
       for (let j = 0; j < productos[i].length; j++) {
         if (productos[i][j].id == parseInt(req.params.id)) {
-          productoSeleccionado = productos[i][j]
+          productoSeleccionado = productos[i][j];
         }
       }
     }
 
     res.render(path.resolve(__dirname, "../views/product.ejs"), {
-      productos: productoSeleccionado
+      productos: productoSeleccionado,
     });
   },
 
   create: (req, res) => {
-    
-    res.render(path.resolve(__dirname, "../views/productCreate.ejs"))
+    res.render(path.resolve(__dirname, "../views/productCreate.ejs"));
   },
 
   save: (req, res) => {
-    
+    console.log(req.files);
     let nombreimagenOrig;
-    let imagenesAdicionales=[];
-    req.files.forEach(element => {
-      if(element.fieldname==="imagenPrincipal"){
-        nombreimagenOrig=retornarNombreArchivo(element.path);
-      }else {
-        imagenesAdicionales.push(retornarNombreArchivo(element.path));
+    let imagenesAdicionales = [];
+    req.files.forEach((element) => {
+      if (element.fieldname === "imagenPrincipal") {
+        nombreimagenOrig = "/img/"+element.filename;
+      } else {
+        imagenesAdicionales.push("/img/"+element.filename);
       }
     });
-    
+
     let idMasAlto = 0;
     for (let i = 0; i < productos.length; i++) {
       for (let j = 0; j < productos[i].length; j++) {
         if (productos[i][j].id > idMasAlto) {
-          idMasAlto = productos[i][j].id
+          idMasAlto = productos[i][j].id;
         }
       }
     }
@@ -60,53 +51,55 @@ let productsController = {
       categoria: req.body.categoria,
       precio: req.body.precio,
       imagen: nombreimagenOrig,
-      masImagenes:imagenesAdicionales
-    }
+      masImagenes: imagenesAdicionales,
+      marca:"",
+      comentarios:[],
+      calificaciones:[]
+    };
     switch (req.body.categoria) {
       case "tecnologia":
-        productos[0].push(productoNuevo)
+        productos[0].push(productoNuevo);
         break;
       case "moda":
-        productos[1].push(productoNuevo)
+        productos[1].push(productoNuevo);
         break;
       case "hogar":
-        productos[2].push(productoNuevo)
+        productos[2].push(productoNuevo);
         break;
       case "deportes":
-        productos[3].push(productoNuevo)
+        productos[3].push(productoNuevo);
         break;
       case "movilidad":
-        productos[4].push(productoNuevo)
+        productos[4].push(productoNuevo);
         break;
       case "ninios":
-        productos[5].push(productoNuevo)
+        productos[5].push(productoNuevo);
         break;
       default:
         break;
     }
 
-    actions.updateProduct(productos);
+    actions.addProduct(productos);
 
     res.redirect("/home");
-  }, 
+  },
 
   comentarios: (req, res) => {
     let comentarioNuevo = req.body.comentario;
-    let calificacionNueva = req.body.calificacion
+    let calificacionNueva = req.body.calificacion;
     console.log(calificacionNueva);
     for (let i = 0; i < productos.length; i++) {
       for (let j = 0; j < productos[i].length; j++) {
         if (productos[i][j].id == parseInt(req.params.id)) {
-          productos[i][j].comentarios.push(comentarioNuevo)
-          productos[i][j].calificaciones.push(calificacionNueva)
+          productos[i][j].comentarios.push(comentarioNuevo);
+          productos[i][j].calificaciones.push(calificacionNueva);
         }
       }
     }
     actions.updateProduct(productos);
 
-    res.redirect("/product/" + req.params.id)
-  }
-
+    res.redirect("/product/" + req.params.id);
+  },
 };
 
 module.exports = productsController;
--- src/data/actions.js
+++ src/data/actions.js
@@ -3,14 +3,19 @@ const productosJson = ('./src/data/productos.json');
 const usuariosJson = ("./src/data/usuarios.json")
 
 const actions = {
+    addProduct: (data) => {
+        fs.writeFileSync(productosJson, JSON.stringify(data, null, 2), (err) => {
+            if (err) throw err;
+        });
+    },
     updateProduct: (data) => {
-        fs.writeFileSync(productosJson, JSON.stringify(data), (err) => {
+        fs.writeFileSync(productosJson, JSON.stringify(data, null, 2), (err) => {
             if (err) throw err;
         });
     },
 
     updateUser: (data) => {
-        fs.writeFileSync(usuariosJson, JSON.stringify(data), (err) => {
+        fs.writeFileSync(usuariosJson, JSON.stringify(data, null, 2), (err) => {
             if (err) throw err;
         });
     },
