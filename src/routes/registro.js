a0ec465fd0f9feb6a1e9e4f198be3826824ee976
jldefigueiredo <67016369+jldefigueiredo@users.noreply.github.com>
2022-05-18 12:07:49 -0300

    se agregaron validaciones y se corrigieron errores

--- src/routes/registro.js
+++ src/routes/registro.js
@@ -6,8 +6,13 @@ const { check } = require("express-validator");
 
 const validateForm = [
     check("usuario").notEmpty().withMessage("Debes elegir un nombre de usuario."),
+    check("usuario").isLength({min:4,max:18}).withMessage("El usuario debe contener entre 4 y 18 carateres"),
     check("mail").isEmail().withMessage("La direccion de email no es valida"),
     check("password").notEmpty().withMessage("Elige tu contraseña"),
+    check("password").isLength({min:6}).withMessage("La contraseña debe tener al menos 6 caracteres"),
+    check("confPassword").notEmpty().withMessage("Debes confirmar la contraseña"),
+    check("confPassword").isLength({min:6}).withMessage("La confirmacion de la contraseña debe tener al menos 6 caracteres"),
+    check("confPassword").equals(check("password")).withMessage("Las contraseñas no coinciden"),
     check("fechanac").notEmpty().withMessage("Ingresa tu fecha de nacimiento")
 ]
 router.get("/", registroController.main);
