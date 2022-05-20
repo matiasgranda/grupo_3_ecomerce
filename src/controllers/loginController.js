f6e5b083ff2ed72539fb1d936a78f187bf90a99e
José Luis de Figueiredo <jldefigueiredo@gmail.com>
2022-05-20 15:38:30 -0300

    Corregido error en login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,7 +4,7 @@ const { parse } = require("path");
 const { Console } = require("console");
 const { validationResult } = require("express-validator");
 const session = require("express-session");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 let loginController = {
   login: (req, res) => {
@@ -15,26 +15,34 @@ let loginController = {
     let errors = validationResult(req);
     if (errors.isEmpty()) {
       let usuarios = JSON.parse(fs.readFileSync("./src/data/usuarios.json"));
-      for (let i=0; i<usuarios.length; i++) {
-          if((usuarios[i].usuario==req.body.user || usuarios[i].mail==req.body.user) && bcrypt.compareSync(req.body.password,usuarios[i].password)){
-              //console.log("ok")
-             // req.session.user=usuario.usuario;
-             // req.session.mail=usuario.mail;
-             // req.session.pais=usuario.pais;
-              session.user=usuarios[i].usuario;
-              session.mail=usuarios[i].mail;
-              session.pais=usuarios[i].pais;
-              return res.redirect('/');
-          } 
+      for (let i = 0; i < usuarios.length; i++) {
+        if (
+          (usuarios[i].usuario == req.body.user ||
+            usuarios[i].mail == req.body.user) &&
+          bcrypt.compareSync(req.body.password, usuarios[i].password)) {
+          //console.log("ok")
+          // req.session.user=usuario.usuario;
+          // req.session.mail=usuario.mail;
+          // req.session.pais=usuario.pais;
+          session.user = usuarios[i].usuario;
+          session.mail = usuarios[i].mail;
+          session.pais = usuarios[i].pais;
+          return res.redirect("/");
+        }
       }
-      let mensaje={
-        msg:"Usuario o contraseña incorrectos"
-      }
-      return res.render(path.resolve(__dirname, "../views/login.ejs"), { errors: errors.mapped(), old: req.body,mensaje:mensaje });
-      
-      
-    }else {
-        return res.render(path.resolve(__dirname, "../views/login.ejs"), { errors: errors.mapped(), old: req.body });
+      let mensaje = {
+        msg: "Usuario o contraseña incorrectos",
+      };
+      return res.render(path.resolve(__dirname, "../views/login.ejs"), {
+        errors: errors.mapped(),
+        old: req.body,
+        mensaje: mensaje,
+      });
+    } else {
+      return res.render(path.resolve(__dirname, "../views/login.ejs"), {
+        errors: errors.mapped(),
+        old: req.body,
+      });
     }
   },
 };
