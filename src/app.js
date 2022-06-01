e266be09acbd9729083ae0d5b535ed43a900dc07
José Luis de Figueiredo <jldefigueiredo@gmail.com>
2022-06-01 14:45:44 -0300

    corregido parametro session

--- src/app.js
+++ src/app.js
@@ -19,6 +19,8 @@ app.use(cookieParser());
 app.use(session({
     name : 'amazona',
     secret : 'amazonasecret1234#',
+    resave: false,
+    saveUninitialized: true,
     cookie : {
             maxAge:(1000 * 60 * 60)
     }      
