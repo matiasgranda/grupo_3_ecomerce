66f4d0c9fe845c5cdf2e51188c1f06336cd9968d
jldefigueiredo <67016369+jldefigueiredo@users.noreply.github.com>
2022-06-08 21:16:16 -0300

    modificada la db

--- src/data/amazona_db.sql
+++ src/data/amazona_db.sql
@@ -1,196 +1,268 @@
-/*
-Navicat MySQL Data Transfer
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: amazona_db
+-- ------------------------------------------------------
+-- Server version	8.0.29
 
-Source Server         : localhost
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : amazona_db
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
+--
+-- Table structure for table `calificaciones`
+--
 
-Date: 2022-06-08 13:21:53
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for calificaciones
--- ----------------------------
 DROP TABLE IF EXISTS `calificaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `calificaciones` (
-  `idcomentario` int(11) NOT NULL AUTO_INCREMENT,
+  `idcomentario` int NOT NULL AUTO_INCREMENT,
   `comentario` text NOT NULL,
-  `idpublicacion` int(11) DEFAULT NULL,
-  `idusuario` int(11) DEFAULT NULL,
-  `calificacion` int(1) NOT NULL DEFAULT 1,
-  `reportado` int(11) DEFAULT 0,
-  `habilitado` int(11) DEFAULT 1,
+  `idpublicacion` int DEFAULT NULL,
+  `idusuario` int DEFAULT NULL,
+  `calificacion` int NOT NULL DEFAULT '1',
+  `reportado` int DEFAULT '0',
+  `habilitado` int DEFAULT '1',
   PRIMARY KEY (`idcomentario`),
   KEY `fk_calificaciones` (`idpublicacion`),
-  CONSTRAINT `fk_calificaciones` FOREIGN KEY (`idpublicacion`) REFERENCES `publicaciones` (`idpublicacion`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_calificaciones` FOREIGN KEY (`idpublicacion`) REFERENCES `publicaciones` (`idpublicacion`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of calificaciones
--- ----------------------------
-INSERT INTO `calificaciones` VALUES ('1', 'Esta muy bueno', '3', '1', '3', '0', '1');
+--
+-- Dumping data for table `calificaciones`
+--
 
--- ----------------------------
--- Table structure for categorias
--- ----------------------------
+LOCK TABLES `calificaciones` WRITE;
+/*!40000 ALTER TABLE `calificaciones` DISABLE KEYS */;
+INSERT INTO `calificaciones` VALUES (1,'Esta muy bueno',3,1,3,0,1);
+/*!40000 ALTER TABLE `calificaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categorias`
+--
+
 DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categorias` (
-  `idcategoria` int(11) NOT NULL AUTO_INCREMENT,
+  `idcategoria` int NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(20) NOT NULL,
-  `habilitado` int(1) NOT NULL DEFAULT 1,
+  `habilitado` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`idcategoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of categorias
--- ----------------------------
-INSERT INTO `categorias` VALUES ('1', 'Tecnología', '1');
+--
+-- Dumping data for table `categorias`
+--
 
--- ----------------------------
--- Table structure for colores
--- ----------------------------
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'Tecnología',1);
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `colores`
+--
+
 DROP TABLE IF EXISTS `colores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `colores` (
-  `idcolor` int(11) NOT NULL AUTO_INCREMENT,
-  `idpublicacion` int(11) NOT NULL,
+  `idcolor` int NOT NULL AUTO_INCREMENT,
+  `idpublicacion` int NOT NULL,
   `color` varchar(12) NOT NULL,
   PRIMARY KEY (`idcolor`),
   KEY `fk_colores` (`idpublicacion`),
-  CONSTRAINT `fk_colores` FOREIGN KEY (`idpublicacion`) REFERENCES `publicaciones` (`idpublicacion`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_colores` FOREIGN KEY (`idpublicacion`) REFERENCES `publicaciones` (`idpublicacion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of colores
--- ----------------------------
+--
+-- Dumping data for table `colores`
+--
 
--- ----------------------------
--- Table structure for domicilios
--- ----------------------------
+LOCK TABLES `colores` WRITE;
+/*!40000 ALTER TABLE `colores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `colores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `domicilios`
+--
+
 DROP TABLE IF EXISTS `domicilios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `domicilios` (
-  `iddomicilios` int(11) NOT NULL AUTO_INCREMENT,
-  `idusuario` int(11) NOT NULL,
+  `iddomicilios` int NOT NULL AUTO_INCREMENT,
+  `idusuario` int NOT NULL,
   `calle` varchar(100) NOT NULL,
-  `altura` int(5) DEFAULT NULL,
-  `piso` int(2) DEFAULT NULL,
+  `altura` int DEFAULT NULL,
+  `piso` int DEFAULT NULL,
   `depto` varchar(3) DEFAULT NULL,
-  `idpais` int(2) NOT NULL,
-  `idprovincia` int(2) NOT NULL,
+  `idpais` int NOT NULL,
+  `idprovincia` int NOT NULL,
   `cp` varchar(6) DEFAULT NULL,
-  `default` int(1) NOT NULL DEFAULT 0,
+  `default` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`iddomicilios`),
   KEY `fk_usuarios_domicilios` (`idusuario`),
-  CONSTRAINT `fk_usuarios_domicilios` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_usuarios_domicilios` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`idusuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of domicilios
--- ----------------------------
+--
+-- Dumping data for table `domicilios`
+--
 
--- ----------------------------
--- Table structure for imagenes
--- ----------------------------
+LOCK TABLES `domicilios` WRITE;
+/*!40000 ALTER TABLE `domicilios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `domicilios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `imagenes`
+--
+
 DROP TABLE IF EXISTS `imagenes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `imagenes` (
-  `idimagen` int(11) NOT NULL AUTO_INCREMENT,
-  `imagenprincipal` int(1) NOT NULL DEFAULT 0,
+  `idimagen` int NOT NULL AUTO_INCREMENT,
+  `imagenprincipal` int NOT NULL DEFAULT '0',
   `imagen` varchar(40) NOT NULL,
-  `idpublicacion` int(11) NOT NULL,
+  `idpublicacion` int NOT NULL,
   PRIMARY KEY (`idimagen`),
   KEY `fk_img_publicaciones` (`idpublicacion`),
-  CONSTRAINT `fk_img_publicaciones` FOREIGN KEY (`idpublicacion`) REFERENCES `publicaciones` (`idpublicacion`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_img_publicaciones` FOREIGN KEY (`idpublicacion`) REFERENCES `publicaciones` (`idpublicacion`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of imagenes
--- ----------------------------
-INSERT INTO `imagenes` VALUES ('1', '1', 'msi1.webp', '3');
-INSERT INTO `imagenes` VALUES ('2', '0', 'msi1.webp', '3');
-INSERT INTO `imagenes` VALUES ('3', '0', 'msi1.webp', '3');
-INSERT INTO `imagenes` VALUES ('4', '0', 'msi1.webp', '3');
+--
+-- Dumping data for table `imagenes`
+--
 
--- ----------------------------
--- Table structure for preguntas
--- ----------------------------
+LOCK TABLES `imagenes` WRITE;
+/*!40000 ALTER TABLE `imagenes` DISABLE KEYS */;
+INSERT INTO `imagenes` VALUES (1,1,'msi1.webp',3),(2,0,'msi1.webp',3),(3,0,'msi2.webp',3),(4,0,'msi3.webp',3);
+/*!40000 ALTER TABLE `imagenes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `preguntas`
+--
+
 DROP TABLE IF EXISTS `preguntas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `preguntas` (
-  `idpregunta` int(11) NOT NULL AUTO_INCREMENT,
-  `idusuario` int(11) NOT NULL,
+  `idpregunta` int NOT NULL AUTO_INCREMENT,
+  `idusuario` int NOT NULL,
   `pregunta` text NOT NULL,
-  `idpublicacion` int(11) DEFAULT NULL,
+  `idpublicacion` int DEFAULT NULL,
   `fechapregunta` date NOT NULL,
-  `reportado` int(11) DEFAULT 0,
+  `reportado` int DEFAULT '0',
   PRIMARY KEY (`idpregunta`),
   KEY `fk_preguntas` (`idpublicacion`),
-  CONSTRAINT `fk_preguntas` FOREIGN KEY (`idpublicacion`) REFERENCES `publicaciones` (`idpublicacion`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_preguntas` FOREIGN KEY (`idpublicacion`) REFERENCES `publicaciones` (`idpublicacion`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of preguntas
--- ----------------------------
-INSERT INTO `preguntas` VALUES ('1', '1', 'Hola funciona con pilas AAA?', '3', '2022-06-08', '0');
+--
+-- Dumping data for table `preguntas`
+--
 
--- ----------------------------
--- Table structure for publicaciones
--- ----------------------------
+LOCK TABLES `preguntas` WRITE;
+/*!40000 ALTER TABLE `preguntas` DISABLE KEYS */;
+INSERT INTO `preguntas` VALUES (1,1,'Hola funciona con pilas AAA?',3,'2022-06-08',0);
+/*!40000 ALTER TABLE `preguntas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `publicaciones`
+--
+
 DROP TABLE IF EXISTS `publicaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `publicaciones` (
-  `idpublicacion` int(11) NOT NULL AUTO_INCREMENT,
+  `idpublicacion` int NOT NULL AUTO_INCREMENT,
   `precio` decimal(8,2) DEFAULT NULL,
-  `descripcion` text DEFAULT NULL,
-  `idusuario` int(11) NOT NULL,
-  `habilitado` int(1) DEFAULT 1,
-  `idcategoria` int(11) NOT NULL,
-  `idsubcategoria` int(11) DEFAULT NULL,
+  `descripcion` text,
+  `idusuario` int NOT NULL,
+  `habilitado` int DEFAULT '1',
+  `idcategoria` int NOT NULL,
+  `idsubcategoria` int DEFAULT NULL,
   `fechapublicacion` date DEFAULT NULL,
-  `reportado` int(1) DEFAULT 0,
+  `reportado` int DEFAULT '0',
   `titulo` varchar(70) NOT NULL,
-  `detallepublicacion` text DEFAULT NULL,
+  `detallepublicacion` text,
   PRIMARY KEY (`idpublicacion`),
   KEY `fk_usuarios` (`idusuario`),
   KEY `fk_categorias` (`idcategoria`),
-  CONSTRAINT `fk_categorias` FOREIGN KEY (`idcategoria`) REFERENCES `categorias` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_usuarios` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_categorias` FOREIGN KEY (`idcategoria`) REFERENCES `categorias` (`idcategoria`),
+  CONSTRAINT `fk_usuarios` FOREIGN KEY (`idusuario`) REFERENCES `usuarios` (`idusuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of publicaciones
--- ----------------------------
-INSERT INTO `publicaciones` VALUES ('3', '75805.00', 'Descripción de prueba', '1', '1', '1', '1', '2022-06-07', '0', 'Titulo de prueba', '<h2>Este sería el titulo del detalle de la publicacion</h2>\r\n<p>un parrafito de prueba</p>\r\n');
+--
+-- Dumping data for table `publicaciones`
+--
 
--- ----------------------------
--- Table structure for respuestas
--- ----------------------------
+LOCK TABLES `publicaciones` WRITE;
+/*!40000 ALTER TABLE `publicaciones` DISABLE KEYS */;
+INSERT INTO `publicaciones` VALUES (3,75805.00,'Descripción de prueba',1,1,1,1,'2022-06-07',0,'Titulo de prueba','<h2>Este sería el titulo del detalle de la publicacion</h2>\r\n<p>un parrafito de prueba</p>\r\n');
+/*!40000 ALTER TABLE `publicaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `respuestas`
+--
+
 DROP TABLE IF EXISTS `respuestas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `respuestas` (
-  `idrespuesta` int(11) NOT NULL AUTO_INCREMENT,
-  `idpregunta` int(11) NOT NULL,
-  `respuesta` text DEFAULT NULL,
+  `idrespuesta` int NOT NULL AUTO_INCREMENT,
+  `idpregunta` int NOT NULL,
+  `respuesta` text,
   `fecharespuesta` date DEFAULT NULL,
-  `reportado` int(1) DEFAULT 0,
+  `reportado` int DEFAULT '0',
   PRIMARY KEY (`idrespuesta`),
   KEY `fk_respuestas` (`idpregunta`),
-  CONSTRAINT `fk_respuestas` FOREIGN KEY (`idpregunta`) REFERENCES `preguntas` (`idpregunta`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Esta es la tabla que contiene las respuestas de las preguntas realizadas por los usuarios';
+  CONSTRAINT `fk_respuestas` FOREIGN KEY (`idpregunta`) REFERENCES `preguntas` (`idpregunta`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COMMENT='Esta es la tabla que contiene las respuestas de las preguntas realizadas por los usuarios';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of respuestas
--- ----------------------------
-INSERT INTO `respuestas` VALUES ('1', '1', 'Hola, no. Tiene batería interna', '2022-06-09', '0');
+--
+-- Dumping data for table `respuestas`
+--
 
--- ----------------------------
--- Table structure for usuarios
--- ----------------------------
+LOCK TABLES `respuestas` WRITE;
+/*!40000 ALTER TABLE `respuestas` DISABLE KEYS */;
+INSERT INTO `respuestas` VALUES (1,1,'Hola, no. Tiene batería interna','2022-06-09',0);
+/*!40000 ALTER TABLE `respuestas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
 DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `idusuario` int(11) NOT NULL AUTO_INCREMENT,
+  `idusuario` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(40) NOT NULL,
   `apellido` varchar(40) NOT NULL,
   `telefono` varchar(15) DEFAULT NULL,
@@ -198,14 +270,30 @@ CREATE TABLE `usuarios` (
   `email` varchar(40) NOT NULL,
   `usuario` varchar(12) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `habilitado` int(1) DEFAULT 1,
+  `habilitado` int DEFAULT '1',
   `imagen` varchar(255) DEFAULT '',
   PRIMARY KEY (`idusuario`,`usuario`),
   KEY `idusuario` (`idusuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of usuarios
--- ----------------------------
-INSERT INTO `usuarios` VALUES ('1', 'José', 'de Figueiredo', '3548978765', '2003-06-12', 'jldefigueiredo@gmail.com', 'Jose', '$2b$10$LTSqR0eQuwCCqzq/xf0./OCwpCwk8BiKttuL1yQtz.T1nNganp7qS', '1', 'default.png');
-INSERT INTO `usuarios` VALUES ('2', 'Matias', 'Granda', '126549876', '2022-05-18', 'matigranda93@gmail.com', 'Mati', '$2b$10$.6G4tI.F9yG.LmlAKeVjKe2sI6qtfEWGuvMU5Vy016FT8uyiKNz2i', '1', 'default.png');
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'José','de Figueiredo','3548978765','2003-06-12','jldefigueiredo@gmail.com','Jose','$2b$10$LTSqR0eQuwCCqzq/xf0./OCwpCwk8BiKttuL1yQtz.T1nNganp7qS',1,'default.png'),(2,'Matias','Granda','126549876','2022-05-18','matigranda93@gmail.com','Mati','$2b$10$.6G4tI.F9yG.LmlAKeVjKe2sI6qtfEWGuvMU5Vy016FT8uyiKNz2i',1,'default.png');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-08 21:14:38
--- src/data/config/config.js
+++ src/data/config/config.js
@@ -1,14 +1,14 @@
 module.exports={
   "development": {
     "username": "root",
-    "password": '',
+    "password": 'ABcd1234',
     "database": "amazona_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": '',
+    "password": 'ABcd1234',
     "database": "amazona_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
