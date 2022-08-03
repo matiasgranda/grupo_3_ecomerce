a84bc762793245003e2d5b3e20b8a278b6415ec1
Matuca <matigranda93@gmail.com>
2022-08-03 13:34:46 -0300

    creando inputs + ruta para poder responder a preguntas en publicaciones

--- src/routes/product.js
+++ src/routes/product.js
@@ -26,6 +26,7 @@ router.get("/:id", productsController.product);
 router.post("/create", fileupload.any(),log,productsController.save);
 router.post("/:id/comentario", productsController.comentarios);
 router.post("/:id/pregunta", productsController.pregunta);
+router.post("/:id/respuesta/:resId")
 router.delete("/delete/:id", productsController.delete);
 router.delete("/delete/publicacion/:idpublicacion/image/:idimage", productsController.deleteImage);
 
