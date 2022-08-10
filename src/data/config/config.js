bc3634717870a57ad0200b0a218bc93e951a7a03
Matuca <matigranda93@gmail.com>
2022-08-10 15:26:34 -0300

    correcciones responsive + db con preguntas, respuestas y comentarios para rellenar

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: "root",
-    password: "ABcd1234",
+    password: "123456",
     database: "amazona_db",
     host: "127.0.0.1",
     dialect: "mysql",
