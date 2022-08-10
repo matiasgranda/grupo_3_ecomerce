bd9f67b1b37dcb3407d14a11d6316014bf90427b
Matuca <matigranda93@gmail.com>
2022-08-10 13:05:09 -0300

    db actualizada, solucionados algunos errores

--- public/js/userDetalleVentas.js
+++ public/js/userDetalleVentas.js
@@ -1,6 +1,6 @@
 const link = document.querySelectorAll(".idCompra");
 const container = document.querySelector(".divDetallesVenta");
-const closeBtn = document.querySelector(".cerrarDetalleVenta");
+const subContainer = document.querySelector(".subContainer");
 const wrapper = document.querySelector(".wrapper");
 
 wrapper.addEventListener("click", function() {
@@ -8,39 +8,41 @@ wrapper.addEventListener("click", function() {
     wrapper.style.display = "none";
 })
 
-closeBtn.addEventListener("click", function() {
-    container.style.display = "none";
-    wrapper.style.display = "none";
-})
 
 link.forEach(link => {
     link.addEventListener("click", async function() {
         
         wrapper.style.display = "flex"
         container.style.display = "flex"
+        subContainer.innerHTML = ""
+        
         try {
             const res = await fetch("http://localhost:3000/perfil/api/"+this.id);
             const data = await res.json()
-            console.log(data)
 
-            data.forEach(compra => {
+            await data.forEach(compra => {
                 const producto = document.createElement("p")
                 const cantidad = document.createElement("p")
                 const precio = document.createElement("p")
-                const subContainer = document.createElement("div")
-                subContainer.classList.add("subContainer")
+                const separador = document.createElement("hr")
+                
+                separador.classList.add("separador")
                 producto.classList.add("itemContainerDetalle")
                 producto.style.fontWeight = 600
                 cantidad.classList.add("itemContainerDetalle")
                 precio.classList.add("itemContainerDetalle")
+                
+                producto.textContent = ""
+                cantidad.textContent = ""
+                precio.textContent = ""
                 producto.textContent = compra.producto
                 cantidad.textContent = "Cantidad: "+compra.cantidad
                 precio.textContent = "Precio unitario: "+compra.precio
                 
-                container.appendChild(subContainer)
                 subContainer.appendChild(producto)
                 subContainer.appendChild(cantidad)
                 subContainer.appendChild(precio)
+                subContainer.appendChild(separador)
             })
 
         } catch (error) {
